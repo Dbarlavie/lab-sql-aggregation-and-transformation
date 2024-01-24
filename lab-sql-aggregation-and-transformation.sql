909ed34c6b6c9a6f7002813680e4264180cbd864
Dbarlavie <dbarlavie@gmail.com>
2024-01-24 18:03:36 +0100

    challange 2 complete

--- lab-sql-aggregation-and-transformation.sql
+++ lab-sql-aggregation-and-transformation.sql
@@ -1,5 +1,5 @@
 use sakila;
-#You need to use SQL built-in functions to gain insights relating to the duration of movies:
+# Challenge 1
 #1.1 Determine the shortest and longest movie durations and name the values as max_duration and min_duration.
 select film_id, length from sakila.film ORDER BY length; 
 select max(length) as max_duration from sakila.film ;
@@ -24,5 +24,22 @@ select *, CONCAT(DATE_FORMAT(rental_date,'%M'))AS 'rental_month', DAYname(rental
 
 select title,rental_duration from sakila.film where ifnull( rental_duration, 'Not Available') order by title asc;
 
-#Please note that even if there are currently no null values in the rental duration column, the query should still be written to handle such cases in the future.
-#Hint: Look for the IFNULL() function.
+#Challenge 2
+# Next, you need to analyze the films in the collection to gain some more insights. Using the film table, determine:
+#1.1 The total number of films that have been released.
+select count(release_year) from sakila.film;
+
+#1.2 The number of films for each rating.
+select rating, count(*) as film_amount from sakila.film group by rating;
+
+#1.3 The number of films for each rating, sorting the results in descending order of the number of films. 
+#This will help you to better understand the popularity of different film ratings and adjust purchasing decisions accordingly.
+select rating, count(*) as film_amount from sakila.film group by rating order by film_amount desc;
+
+#Using the film table, determine:
+#2.1 The mean film duration for each rating, and sort the results in descending order of the mean duration. 
+#Round off the average lengths to two decimal places. This will help identify popular movie lengths for each category.
+select rating, count(*) as film_amount, round(avg(length),2) as mean_film_duration from sakila.film group by rating order by mean_film_duration desc;
+
+#2.2 Identify which ratings have a mean duration of over two hours in order to help select films for customers who prefer longer movies.
+select rating, count(*) as film_amount, round(avg(length),2) as mean_film_duration from sakila.film group by rating having mean_film_duration>120 order by mean_film_duration desc ;
